cbb167acae451c7b92e9a7646817009eb8f9d64e
artiship <lichuanliang@126.com>
2018-01-11 22:39:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <div class="wrapper">
+      <div class="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, expedita!
+      </div>
+      <div class="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, porro.
+      </div>
+      <div class="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, alias.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'app-header': Header
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
