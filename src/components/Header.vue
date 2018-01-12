cfbe8d9bec7c9a9e0aa9e210dd9667a98607e651
artiship <lichuanliang@126.com>
2018-01-12 21:35:26 +0800

    trying to add vue router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
-<template lang="html">
+«»<template lang="html">
   <b-navbar toggleable="md" type="dark" variant="info">
   <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
   <b-navbar-brand href="#">NavBar</b-navbar-brand>
   <b-collapse is-nav id="nav_collapse">
     <b-navbar-nav>
-      <b-nav-item href="#">Link</b-nav-item>
-      <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="/about">About</b-nav-item>
     </b-navbar-nav>
     <!-- Right aligned nav items -->
     <b-navbar-nav class="ml-auto">
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import router from './router'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
-Vue.use(Bootstrap)
+console.log(router);
+
 Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
